d2a37738a08d33af9368770996a8313ac55ecd6b
nirzaf <nirzaf@gmail.com>
2019-02-14 16:38:16 +0530

    Update new

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,6 +1,16 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $(document).ready(function () {
+        $('.progress-bar').waypoint(function () {
+          $('.progress-bar').css({
+            animation: "animate-positive 2s",
+            opacity: "1"
+          });
+        }, {
+          offset: '75%'
+        });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
