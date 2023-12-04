ede38931fd99b54623bd26d79941459feeecdeea
nirzaf <mfmfazrin1986@gmail.com>
2023-12-04 23:53:49 +0300

    Delete unused files and images

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Dot Net Evangelist | Home';
-export const SITE_DESCRIPTION = 'Welcome to blog, where I share my thoughts on software development, .NET, and more.';
