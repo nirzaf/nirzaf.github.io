e842ff558b3ced07a139cae19b0bf967b7832870
nirzaf <mfmfazrin1986@gmail.com>
2023-12-04 23:53:49 +0300

    Delete unused files and images

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-	site: 'https://nirzaf.github.io/',
-	integrations: [mdx(), sitemap()],
-});
