fce7d230e3acca7ffa39f2f3cb221a4f78e58f0f
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-01 22:34:51 +0300

    feat: Update site metadata and author information (#)
    This commit updates the site metadata, including the author name,
    site title, and description. The changes provide accurate and
    informative details about the site and its content.

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'M.F.M Fazrin', // Site author
+	title: 'DotNet Blogs by Dotnet Evangelist ', // Site title.
+	description: 'Explore the latest insights, tutorials, and best practices in .NET development from a passionate Dotnet Evangelist. Stay up-to-date with cutting-edge technologies and enhance your skills in C#, ASP.NET, and more.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
