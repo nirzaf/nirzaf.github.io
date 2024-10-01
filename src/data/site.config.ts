8ca9fe5e50ea84302424f4a0896d1204bb0b2b0b
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-01 22:37:12 +0300

    feat: Update pagination size to 9 posts per page

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -15,5 +15,5 @@ export const siteConfig: SiteConfig = {
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	paginationSize: 9 // Number of posts per page
 }
