4e1663aa6c6f6dc4a6b5141a4378cc52d55f0963
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-04 01:27:41 +0300

    feat: Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nirzaf.github.io',
+	site: 'https://dotnetevangelist.net',
 	trailingSlash: 'never',
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
