31dbbbe409a438a147357a24a698a3ca195e3462
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-08 19:47:20 +0300

    category images added

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,14 +1,23 @@
-import dotnetImage from '../assets/images/categories/dotnet.png';
+import dotnetImage from '../assets/images/categories/dotnet.jpg';
+import efcore from '../assets/images/categories/efcore.jpg';
+import aspnetcore from '../assets/images/categories/aspnet.jpg';
+import designPatterns from '../assets/images/categories/design-patterns.jpg';
+import blazor from '../assets/images/categories/blazor.jpg';
+import csharp from '../assets/images/categories/csharp.jpg';
+import ui from '../assets/images/categories/ui-libraries.jpg';
+import azure from '../assets/images/categories/azure.jpg';
+import o365 from '../assets/images/categories/o365.jpg';
+
+
 
 export const CATEGORIES = [
-	{ name: 'EFCore', image: dotnetImage },
-	{ name: 'Design Patterns', image: dotnetImage },
-	{ name: 'ASP.NET Core', image: dotnetImage },
+	{ name: 'EFCore', image: efcore },
+	{ name: 'Design Patterns', image: designPatterns },
+	{ name: 'ASP.NET Core', image: aspnetcore },
 	{ name: '.NET', image: dotnetImage },
-	{ name: 'Blazor', image: dotnetImage },
-	{ name: 'Csharp', image: dotnetImage },
-	{ name: 'UI', image: dotnetImage },
-	{ name: 'Programming', image: dotnetImage },
-	{ name: 'Azure', image: dotnetImage },
-	{ name: 'O365', image: dotnetImage }
+	{ name: 'Blazor', image: blazor },
+	{ name: 'Csharp', image: csharp },
+	{ name: 'UI', image: ui },
+	{ name: 'Azure', image: azure },
+	{ name: 'O365', image: o365 }
 ] as const;
