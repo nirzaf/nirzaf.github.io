a353040eca3681fbafa017975362a14ea359d287
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-11 21:12:46 +0300

    feat: updates blog site and moves blog-template.mdx to src/content/blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nirzaf.gitlab.io/nirzaf',
+	site: 'https://dotnetevangelist.com',
 	trailingSlash: 'never',
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
