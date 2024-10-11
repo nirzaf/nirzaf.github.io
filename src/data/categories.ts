5fe31f8a37f723473b39d33e29b13e85dbccc2e8
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-11 17:38:37 +0300

    ``` feat: Add new blog post and update categories (master)
    Update blog post categories to 'Database' instead of
    'EFCore'. Add a new blog post about mastering SQL
    using SUM() with CASE WHEN.  Update .idea
    workspace file and reset mode to HARD.
    ```

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,5 +1,5 @@
 import dotnetImage from '../assets/images/categories/dotnet.jpg';
-import efcore from '../assets/images/categories/efcore.jpg';
+import database from '../assets/images/categories/efcore.jpg';
 import aspnetcore from '../assets/images/categories/aspnet.jpg';
 import designPatterns from '../assets/images/categories/design-patterns.jpg';
 import blazor from '../assets/images/categories/blazor.jpg';
@@ -11,7 +11,7 @@ import o365 from '../assets/images/categories/o365.jpg';
 
 
 export const CATEGORIES = [
-	{ name: 'EFCore', image: efcore },
+	{ name: 'Database', image: database },
 	{ name: 'Design Patterns', image: designPatterns },
 	{ name: 'ASP.NET Core', image: aspnetcore },
 	{ name: '.NET', image: dotnetImage },
