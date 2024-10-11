fb7c8fcb079a77942f82de42f5cca2d2075fbd97
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-11 22:37:16 +0300

    feat(backup): add automated backup branch creation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dotnetevangelist.com',
 	trailingSlash: 'never',
+	output: 'hybrid', // Add this line
+	adapter: vercel(),  // Add this line
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
