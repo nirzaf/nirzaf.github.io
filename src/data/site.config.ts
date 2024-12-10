fe70a0d2554c1aed7cb7c275080b2ee382eb69d7
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-12-10 21:36:52 +0300

    🛠️ src/data/site.config.ts -> Updated site title and description 🛠️ src/layouts/BaseLayout.astro -> Simplified head and body styles

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -1,19 +1,20 @@
 interface SiteConfig {
-	author: string
-	title: string
-	description: string
-	lang: string
-	ogLocale: string
-	shareMessage: string
-	paginationSize: number
+    author: string
+    title: string
+    description: string
+    lang: string
+    ogLocale: string
+    shareMessage: string
+    paginationSize: number
+
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'M.F.M Fazrin', // Site author
-	title: '.Net Evangelist | Explore & Enhance Your Skills in .Net & More ', // Site title.
-	description: 'Explore the latest insights, tutorials, and best practices in .NET development from a passionate Dotnet Evangelist. Stay up-to-date with cutting-edge technologies and enhance your skills in C#, ASP.NET, and more.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 9 // Number of posts per page
+    author: 'M.F.M Fazrin', // Site author
+    title: 'DotNet Blogs by Dotnet Evangelist ', // Site title.
+    description: 'Expert tutorials and insights on .NET development, covering C#, ASP.NET, and best practices. Learn from a passionate Dotnet Evangelist.', // Description to display in the meta tags
+    lang: 'en-GB',
+    ogLocale: 'en_GB',
+    shareMessage: 'Share this post', // Message to share a post on social media
+    paginationSize: 9 // Number of posts per page
 }
